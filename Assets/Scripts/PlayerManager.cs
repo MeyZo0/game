666cb51a4d61bf3c001903d2ec220a57b673d81e
XomsEdu <vakhrameiev.b1206@lntu.edu.ua>
2024-02-22 15:54:50 +0200

    Update prototype
    - Пофіксив "липкі стіни"
    - Допрацював стрибок
    - Додав даблджамп (к-кість стрибків можна міняти в едіторі)
    - Невеликі зміни в попередніх настройках відповідно до змін на сцені.

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -13,12 +13,32 @@ public class PlayerManager : MonoBehaviour
 
     public float moveSpeed;
     public float jumpForce;
-    public float groundRaycast;
+    public int jumpAmount = 2;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    private int jumpCount;
+
+
+    public Vector2 groundBoxCast;
+    public float castDistance;
 
     private void Awake()
         {
             inputManager = GetComponent<InputManager>();
             rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+    private void Update()
+        {
+            if (rigidbody.velocity.y < 0) 
+                {
+                    rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime;
+                }
+            else if (rigidbody.velocity.y > 0 && !Input.GetButton ("Jump")) 
+                {
+                    rigidbody.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
+                }
         }
 
     private void FixedUpdate()
@@ -39,27 +59,42 @@ public class PlayerManager : MonoBehaviour
         
         }
 
+    private void OnDrawGizmos()
+        {
+            Vector2 boxOrigin = transform.position;
+            Vector2 boxDirection = -transform.up;
+
+            // Draw a wireframe box to represent the ground check area
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(boxOrigin - new Vector2(0f, castDistance / 2f), groundBoxCast);
+        }
+
     private void GroundCheck()
-    {
-        Vector2 rayOrigin = new Vector2(transform.position.x, transform.position.y);
-
-        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.down, groundRaycast, ground);
-
-        if (hit.collider != null)
         {
-            isGrounded = true;
+            Vector2 boxOrigin = transform.position;
+            Vector2 boxDirection = -transform.up;
+
+            RaycastHit2D hit = Physics2D.BoxCast(boxOrigin, groundBoxCast, 0f, boxDirection, castDistance, ground);
+
+            if (hit.collider != null)
+            {
+                isGrounded = true;
+                jumpCount = jumpAmount;
+            }
+            else
+            {
+                isGrounded = false;
+            }
         }
-        else
-        {
-            isGrounded = false;
-        }
-    }
+
 
     private void PlayerJump()
         {
-            if (inputManager.isJump && isGrounded)
+            if (inputManager.isJump && jumpCount > 0)
                 {
                     rigidbody.velocity = Vector2.up * jumpForce;
+
+                    jumpCount -= 1;
 
                     inputManager.isJump = false;
                 }
